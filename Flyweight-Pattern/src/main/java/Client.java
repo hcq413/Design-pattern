26afd019ee783c7e88a77ba6c4bc898f0c824ea8
hcq413 <1725163396@qq.com>
2022-02-08 11:09:33 +0800

    2022-2-27 1.0

--- Flyweight-Pattern/src/main/java/Client.java
+++ Flyweight-Pattern/src/main/java/Client.java
@@ -6,20 +6,14 @@
  */
 public class Client {
 
-    private static User user;
-
     public static void main(String[] args) {
-        user = new User();
         WebSiteFactory factory = new WebSiteFactory();
         WebSite hcq = factory.getWebsite("网站");
         WebSite hcq1 = factory.getWebsite("博客");
         WebSite hcq2 = factory.getWebsite("cdn");
-        user.setName("hcq1");
-        hcq.use(user);
-        user.setName("hcq2");
-        hcq1.use(user);
-        user.setName("hcq3");
-        hcq2.use(user);
+        hcq.use(new User("张三"));
+        hcq1.use(new User("李四"));
+        hcq2.use(new User("王五"));
         System.out.println("factory = " + factory.websitecount());
     }
 }
