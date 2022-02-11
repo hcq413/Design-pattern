5923f6de90488a389b3d2002bcbf827d8ece0c15
hcq413 <1725163396@qq.com>
2022-02-11 22:32:09 +0800

    命令模式 1.2

--- Command-Pattern/src/main/java/com/hcq/xxx/v1/Client.java
+++ Command-Pattern/src/main/java/com/hcq/xxx/v1/Client.java
@@ -7,12 +7,13 @@ public class Client {
         LightOffCommand offCommand = new LightOffCommand(receiver);
 
         RemoteController controller = new RemoteController();
-        controller.setCommand(0,onCommand,offCommand);
-        System.out.println("按下电灯开的按钮");
-        controller.onButtonWasPushed(0);
+        controller.setCommand(0, onCommand, offCommand);
         System.out.println("按下电灯关的按钮");
+        controller.onButtonWasPushed(0);
+        System.out.println("按下电灯开的按钮");
         controller.offButtonWasPushed(0);
         System.out.println("按下撤销按钮");
         controller.undoButtonWasPushed();
+
     }
 }
